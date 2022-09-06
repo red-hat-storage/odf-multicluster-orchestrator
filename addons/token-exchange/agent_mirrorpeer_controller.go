5adf18de6fda9a8e731fb315dcbdc2a4a53d3ef4
Vineet Badrinath <vineetbnath@gmail.com>
2022-09-06 20:13:06 +0530

    Bump k8s (v0.25) and update other deps
    This commit updates the following deps
    - k8s from 0.23 to 0.25
    - changes import path for volume replication operator to new repository
      (github.com/csi-addons/kubernetes-csi-addons v0.5.0)
    - updates to openshift api to latest commits
    - exclude other incompatible apis
    
    Signed-off-by: Vineet Badrinath <vineetbnath@gmail.com>

--- addons/token-exchange/agent_mirrorpeer_controller.go
+++ addons/token-exchange/agent_mirrorpeer_controller.go
@@ -24,7 +24,7 @@ import (
 	"sort"
 	"strconv"
 
-	replicationv1alpha1 "github.com/csi-addons/volume-replication-operator/api/v1alpha1"
+	replicationv1alpha1 "github.com/csi-addons/kubernetes-csi-addons/apis/replication.storage/v1alpha1"
 	obv1alpha1 "github.com/kube-object-storage/lib-bucket-provisioner/pkg/apis/objectbucket.io/v1alpha1"
 	ocsv1 "github.com/red-hat-storage/ocs-operator/api/v1"
 	multiclusterv1alpha1 "github.com/red-hat-storage/odf-multicluster-orchestrator/api/v1alpha1"
