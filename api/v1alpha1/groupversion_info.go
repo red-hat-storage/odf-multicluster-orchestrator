bd23569a02083aad00ecd9a6636dfd8a37bcdd76
Umanga Chapagain <chapagainumanga@gmail.com>
2023-04-05 16:51:20 +0530

    fixing go fmt issues
    Signed-off-by: Umanga Chapagain <chapagainumanga@gmail.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the multicluster v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=multicluster.odf.openshift.io
+// +kubebuilder:object:generate=true
+// +groupName=multicluster.odf.openshift.io
 package v1alpha1
 
 import (
