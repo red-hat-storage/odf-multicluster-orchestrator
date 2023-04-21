ba449c3e3d93e456fc2ff027e55a509c53cb0ef6
Vineet Badrinath <vineetbadrinath@li-2d63354c-332f-11b2-a85c-ed72ecf677ea.ibm.com>
2023-04-21 17:32:15 +0530

    Fixes for MMode reconciler
    This commit fixes the following issues -
    
    1. Conflict error issues with the finalizer
    2. Checks if StorageCluster mirroring is enabled before scaling.
    3. Status update done at the end of reconciliation
    4. Returns only a single cephcluster instead of an array as the targetId should only return single unique cephcluster

--- controllers/utils/cluster.go
+++ controllers/utils/cluster.go
@@ -2,7 +2,10 @@ package utils
 
 import (
 	"context"
+	"fmt"
 	ocsv1 "github.com/red-hat-storage/ocs-operator/api/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -10,9 +13,10 @@ import (
 type ClusterType string
 
 const (
-	CONVERGED ClusterType = "Converged"
-	EXTERNAL  ClusterType = "External"
-	UNKNOWN   ClusterType = "Unknown"
+	CONVERGED                     ClusterType = "Converged"
+	EXTERNAL                      ClusterType = "External"
+	UNKNOWN                       ClusterType = "Unknown"
+	CephClusterReplicationIdLabel             = "replicationid.multicluster.openshift.io"
 )
 
 func GetClusterType(storageClusterName string, namespace string, client client.Client) (ClusterType, error) {
@@ -25,4 +29,35 @@ func GetClusterType(storageClusterName string, namespace string, client client.C
 		return EXTERNAL, nil
 	}
 	return CONVERGED, nil
+}
+
+func ScaleDeployment(ctx context.Context, client client.Client, deploymentName string, namespace string, replicas int32) error {
+	var deployment appsv1.Deployment
+	err := client.Get(ctx, types.NamespacedName{Namespace: namespace, Name: deploymentName}, &deployment)
+	if err != nil {
+		if k8serrors.IsNotFound(err) {
+			return fmt.Errorf("deployment %s not found in namespace %s", deploymentName, namespace)
+		}
+		return err
+	}
+	deployment.Spec.Replicas = &replicas
+	err = client.Update(ctx, &deployment)
+	if err != nil {
+		return err
+	}
+
+	var updatedDeployment appsv1.Deployment
+	for {
+		err := client.Get(context.TODO(), types.NamespacedName{
+			Namespace: namespace, Name: deploymentName,
+		}, &updatedDeployment)
+		if err != nil {
+			return err
+		}
+		if updatedDeployment.Status.Replicas == replicas {
+			break
+		}
+	}
+
+	return nil
 }
