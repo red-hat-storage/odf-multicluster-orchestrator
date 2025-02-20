9f13c253dc51e680669fb96c8e33902a8c8dc788
Umanga Chapagain <uchapaga@redhat.com>
2025-02-20 15:36:47 +0530

    use same S3 naming scheme for internal and provider mode
    Using different naming scheme for internal and provider mode could
    cause issues later on if/when users want to move between modes. So,
    this commit reverts the special handling for provider mode during
    OBC name generation and S3 profile name generation.
    
    Signed-off-by: Umanga Chapagain <uchapaga@redhat.com>

---
+++ controllers/utils/s3_test.go
@@ -0,0 +1,75 @@
+package utils
+
+import (
+	"testing"
+
+	multiclusterv1alpha1 "github.com/red-hat-storage/odf-multicluster-orchestrator/api/v1alpha1"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateBucketName(t *testing.T) {
+	mirrorPeer := multiclusterv1alpha1.MirrorPeer{
+		Spec: multiclusterv1alpha1.MirrorPeerSpec{
+			Items: []multiclusterv1alpha1.PeerRef{
+				{
+					ClusterName: "cluster1",
+					StorageClusterRef: multiclusterv1alpha1.StorageClusterRef{
+						Name:      "ocs-storagecluster",
+						Namespace: "openshift-storage",
+					},
+				},
+				{
+					ClusterName: "cluster2",
+					StorageClusterRef: multiclusterv1alpha1.StorageClusterRef{
+						Name:      "ocs-storagecluster",
+						Namespace: "openshift-storage",
+					},
+				},
+			},
+		},
+	}
+	bucket := GenerateBucketName(mirrorPeer)
+	assert.Equal(t, "odrbucket-b1b922184baf", bucket)
+
+	mirrorPeer = multiclusterv1alpha1.MirrorPeer{
+		Spec: multiclusterv1alpha1.MirrorPeerSpec{
+			Items: []multiclusterv1alpha1.PeerRef{
+				{
+					ClusterName: "cluster1",
+					StorageClusterRef: multiclusterv1alpha1.StorageClusterRef{
+						Name: "ocs-storagecluster",
+					},
+				},
+				{
+					ClusterName: "cluster2",
+					StorageClusterRef: multiclusterv1alpha1.StorageClusterRef{
+						Name: "ocs-storagecluster",
+					},
+				},
+			},
+		},
+	}
+	bucket = GenerateBucketName(mirrorPeer)
+	assert.Equal(t, "odrbucket-b1b922184baf", bucket)
+
+	mirrorPeer = multiclusterv1alpha1.MirrorPeer{
+		Spec: multiclusterv1alpha1.MirrorPeerSpec{
+			Items: []multiclusterv1alpha1.PeerRef{
+				{
+					ClusterName: "cluster3",
+					StorageClusterRef: multiclusterv1alpha1.StorageClusterRef{
+						Name: "ocs-storagecluster",
+					},
+				},
+				{
+					ClusterName: "cluster4",
+					StorageClusterRef: multiclusterv1alpha1.StorageClusterRef{
+						Name: "ocs-storagecluster",
+					},
+				},
+			},
+		},
+	}
+	bucket = GenerateBucketName(mirrorPeer)
+	assert.Equal(t, "odrbucket-db0233c5db46", bucket)
+}
